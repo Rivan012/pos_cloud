a8c842886ab956fec25499d75ed11f3e633dd2a1
Alif Cowboy <alifalfarizi176@gmail.com>
2026-05-20 14:36:27 +0700

    Membuat API modul diskon

--- app/Http/Controllers/Api/DiskonController.php
+++ app/Http/Controllers/Api/DiskonController.php
@@ -8,11 +8,13 @@ use App\Http\Controllers\Controller;
 
 class DiskonController extends Controller
 {
+    // Menampilkan semua data diskon //
     public function index()
     {
         return response()->json(Diskon::all());
     }
 
+    // menambahkan data diskon baru //
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -20,22 +22,22 @@ class DiskonController extends Controller
             'jenis_diskon'=>'required',
             'nilai_diskon'=>'required'
         ]);
-
+    // menyimpan data diskon baru ke database //
         return response()->json(Diskon::create($data), 201);
     }
-
+    // menampilkan detail diskon berdasarkan id //
     public function show(Diskon $diskon)
     {
         return response()->json($diskon);
     }
-
+    // mengupdate data diskon //
     public function update(Request $request, Diskon $diskon)
     {
         $diskon->update($request->all());
 
         return response()->json($diskon);
     }
-
+    // menghapus data diskon //
     public function destroy(Diskon $diskon)
     {
         $diskon->delete();
